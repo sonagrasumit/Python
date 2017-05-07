baa870e8cc62c3e5966855693affff6f06c0b35e
James Mc Dermott <james.mcdermott89@gmail.com>
2017-05-07 19:29:16 +0100

    Update material.py

--- _BeginnerMaterial_/material.py
+++ _BeginnerMaterial_/material.py
@@ -1,16 +1,52 @@
+# print() is a statement that outputs data to the screen
+# something inside quotation marks is called a String
+print("Hello World")
 
-# print is statement which returns what lies inside the brackets
-# print returns "hello world" in the statement below
+'''
+This is a multi-line comment
+'''
 
-print("hello world")
+# A variable is a place to store a value
+# Its name is like a label for that value
 
-# "hello world" is in quotations marks.
-# Something inside quotation marks is called a String.
+name = "So You Want To Code"
 
+print(name)
 
-greeting = "hello world"
-# this String is now stored inside the word greeting.
-# something that stores something inside of it like this is called a Variable
+# A variable name can hold letters, numbers, or special characters
+# variable names cannot start with a number
 
-print(greeting)
-# Like Strings, we can print Variables too!
+# There are five built in data types: Numbers, Strings, List, Tuple, Dictionary
+# You can store any data type in any variable
+
+name = 15
+print(name)
+
+# There are a few arithmetic operators
+# they are:   +, -, *, /, %, **, //
+# ** is Exponential
+# // is Floor Division
+
+print("2 + 2 =", 2+2)
+print("4 - 2 =", 4-2)
+print("5 * 2 =", 5*2)
+print("6 / 2 =", 6/2)
+print("7 % 2 =", 7%2)
+print("2 ** 2 =", 2**2)
+print("9 // 2 =", 9//2)
+
+#  * and / are always performed before + and -
+
+# A string is a sequence of characters surrounded by " or '
+# A multi-line quote
+multi_line_quote = ''' multi-line 
+quote " '''
+
+# To embed a string in output use %s
+print("%s %s" % ('This is a',  multi_line_quote))
+
+# To keep from printing newlines use end=""
+print("I don't like ",end="")
+
+# printing a string multiple times with *
+print('\n' * 5)
