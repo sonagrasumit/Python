6c9418c331e9751eaddd8229ba231ef995362115
James Mc Dermott <james.mcdermott89@gmail.com>
2017-05-07 19:13:59 +0100

    Update and rename hello_world.py to material.py

---
+++ _BeginnerMaterial_/material.py
@@ -0,0 +1,16 @@
+
+# print is statement which returns what lies inside the brackets
+# print returns "hello world" in the statement below
+
+print("hello world")
+
+# "hello world" is in quotations marks.
+# Something inside quotation marks is called a String.
+
+
+greeting = "hello world"
+# this String is now stored inside the word greeting.
+# something that stores something inside of it like this is called a Variable
+
+print(greeting)
+# Like Strings, we can print Variables too!
